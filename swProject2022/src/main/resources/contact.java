8e1b33b39dde69a86a378539f047fc8dd462236e
halaokal <hala_okal@hotmail.com>
2022-10-27 16:31:21 +0300

    halacommit1

---
+++ swProject2022/src/main/resources/contact.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+
+public class contact {
+	String s;
+public void PrintListusers(ArrayList<User> u) {
+	for(int i=0;i<u.size();i++) {
+		System.out.println(u.get(i).toString());
+}
+
+}
+public void Printuser (User u) {
+	s=b.toString();
+	System.out.println(s);
+}
+}
